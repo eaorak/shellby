0faf6b3baef111db752d3c1a56a9132f87894954
Ender Aydin Orak <ender.orak@johnlewis.co.uk>
2021-07-28 18:14:31 +0100

    wip

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 #!/usr/bin/env node
 
+// https://itnext.io/how-to-create-your-own-typescript-cli-with-node-js-1faf7095ef89
+
 const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
 const path = require('path');
 const program = require('commander');
+const { runInAuth, listEvents } = require('./calendar')
 
 clear();
 console.log(
@@ -15,18 +18,18 @@ console.log(
 
 program
   .version('0.0.1')
-  .description("CLI to unite them all")
+  .description("Google calendar cli")
   .option('-a, --add <event-name>', 'Add event')
   .option('-d, --delete', 'Delete event')
   .option('-l, --list', 'List events')
   .parse(process.argv)
 
-console.log('process.argv: ', process.argv)
-
 const options = program.opts()
-console.log('options: ', options)
-console.log('Event name: ', options.add)
 
 if (!process.argv.slice(2).length) {
   program.outputHelp()
+}
+
+if (options.list) {
+  runInAuth(listEvents)
 }
